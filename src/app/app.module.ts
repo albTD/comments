bbda3d1f02d44ccbbde822fa9bcedeb0b893ce4c
albTD <albinsam8081@gmail.com>
2020-04-23 09:42:20 +0530

    my very first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { HttpClient} from '@angular/common/http';
+// import{Platform} from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public tap: number = 0;
+  results:any=[];
+  comments = [];
+  message: string;
+pid:'';
+uid: '';
+  constructor(public navCtrl: NavController, public http:HttpClient) {
 
-  constructor(public navCtrl: NavController) {
+this.getComment();
+  }
+sendComment(){
+    if(this.message != ''){
+      
+      var data={postid:this.pid,userid:this.uid,comment:this.message};
+      
+      this.http.post('http://localhost:9999/tb2',data).subscribe((res : any) => {
+        // this.comments.push(this.message);
+   
+        this.message = '';
+       this.uid = '';
+       this.pid = '';
+       
+      })
+      console.log('as');
+    this.getComment();
+    }
 
+  }
+  getComment(){
+   
+      this.http.get('http://localhost:9999/tb1').subscribe((res : any) => {
+        
+   this.results=res;
+console.log('done');
+      })
+  }
+  ionViewDidLoad(){
+ 
+  
+  
+  }
+  tapEvent(e) {
+    this.tap++
   }
 
 }
